25701b7f8342184403fb393d17ec79072c51828b
serhatprogramming <serhatprogramming@gmail.com>
2022-08-28 16:55:18 -0400

    error middleware, mongodb connection added.

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-console.log("Server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5500;
+
+// Connect to Database
+connectDB();
+
+const app = express();
+
+// to reach post request body content in json
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
