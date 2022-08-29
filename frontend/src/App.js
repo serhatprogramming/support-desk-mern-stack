00d95b6353f4a7292053780c34da558267e887b6
serhatprogramming <serhatprogramming@gmail.com>
2022-08-28 22:42:55 -0400

    front end initial setup and pages created.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
 function App() {
-  return <div>My App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
